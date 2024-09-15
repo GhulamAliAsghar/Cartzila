3e1d04073a909d67660f89cc7595de75cb1e4194
GhulamAliAsghar <galiasghar9.xwave@gmail.com>
2024-09-16 01:30:22 +0500

    form validation with API

---
+++ scripts/login.js
@@ -0,0 +1,34 @@
+async function apiData(params) {
+    let userData = null
+
+
+    try {
+        let users = await fetch("https://fakestoreapi.com/users")
+        userData = await users.json()
+        console.log(userData);
+    } catch (error) {
+
+    }
+
+    document.getElementById("loginForm").addEventListener("submit", function (e) {
+        e.preventDefault()
+
+        let mail = document.getElementById("userMail").value
+        let password = document.getElementById("userPassword").value
+
+        let searchUser = userData.find(user => user.email===mail && user.password===password)
+
+        if (searchUser) {
+            window.location.href = "/index.html";
+        } else {
+            alert("Email or Password not found")
+        }
+
+    })
+
+}
+
+apiData()
+
+
+
