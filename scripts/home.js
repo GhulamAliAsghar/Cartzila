043a128e8d2e989e6cd8bc6aac72eafc70d773f5
GhulamAliAsghar <galiasghar9.xwave@gmail.com>
2024-09-18 23:52:31 +0500

    --

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,21 @@
+// let cartBtn = document.getElementById("cart-open")
+// let sideBar = document.getElementById("cart")
+
+// cartBtn.addEventListener('click' , function(params) {
+//     sideBar.style.display = "block"
+// }) 
+
+let modal= document.getElementById("cart")
+let Login= document.getElementById("cart-open")
+let close= document.getElementById("cart-close")
+
+Login.addEventListener("click" , function () {
+    modal.style.display ="block"
+})
+
+close.addEventListener("click" , function () {
+    modal.style.display ="none"
+})
+
+console.log("hello world");
 
